ef25db67a05adaeffd9acee8ae3245032856cfc5
marknjoroge <marknnjorogeh@gmail.com>
2022-09-03 14:08:36 +0300

    done: :

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,15 +7,15 @@ export default function NavBar() {
         if (window.pageYOffset == 0) {
             // document.getElementById("navbar").style.position = "fixed";
             // document.getElementById("bottom-nav-part").style.top = "0px";
-    //         // document.getElementById("top-nav-part").style.top = "0px";
-    //         document.getElementById("bottom-nav-part").style.width = "90%";
-    //         document.getElementById("bottom-nav-part").style.backgroundColor = "#C6CEBF";
-    //     } else {
-    //         document.getElementById("bottom-nav-part").style.top = "0px";
-    //         document.getElementById("bottom-nav-part").style.width = "100%";
-    //         document.getElementById("bottom-nav-part").style.borderRadius = "0";
-    //         // document.getElementById("top-nav-part").style.top = "-90px";
-    //         document.getElementById("bottom-nav-part").style.backgroundColor = "white";
+            //         // document.getElementById("top-nav-part").style.top = "0px";
+            //         document.getElementById("bottom-nav-part").style.width = "90%";
+            //         document.getElementById("bottom-nav-part").style.backgroundColor = "#C6CEBF";
+            //     } else {
+            //         document.getElementById("bottom-nav-part").style.top = "0px";
+            //         document.getElementById("bottom-nav-part").style.width = "100%";
+            //         document.getElementById("bottom-nav-part").style.borderRadius = "0";
+            //         // document.getElementById("top-nav-part").style.top = "-90px";
+            //         document.getElementById("bottom-nav-part").style.backgroundColor = "white";
         }
     }
 
@@ -24,18 +24,18 @@ export default function NavBar() {
             <div class="container">
                 <div id="top-nav-part">
                     <div>
-                        <a id="home-link" href="#home">DoMyCoding.</a>
+                        <a id="home-link" href="/">DoMyCoding.</a>
                     </div>
                 </div>
                 <hr />
                 <div id="bottom-nav-part">
 
                     <div class="innernav">
-                        <a class="nav-link" href="#about">How it works</a>
-                        <a class="nav-link" href="#academics">Contact us</a>
-                        <a class="nav-link" href="#portfolio">Pricing</a>
-                        <a class="nav-link" href="#contact">FAQ</a>
-                        <a class="nav-link" href="#contact">Guarantees</a>
+                        <a class="nav-link" href="/howitworks">How it works</a>
+                        <a class="nav-link" href="/contact">Contact us</a>
+                        <a class="nav-link" href="/pricing">Pricing</a>
+                        <a class="nav-link" href="/faq">FAQ</a>
+                        <a class="nav-link" href="/guarantees">Guarantees</a>
                     </div>
                 </div>
             </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,29 +5,29 @@ import './index.css';
 
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route
 } from "react-router-dom";
 
 import HomePage from './Home';
-import Test from './Test'
 import ContactUs from './ContactUs';
 import FAQ from './FAQ';
 import Guarantees from './Guarantees';
+import HowItWorks from './HowItWorks';
+import Pricing from './Pricing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Router>
-      <Switch>
-        <Route path="/"> */}
-    <HomePage />
-    {/* <ContactUs></ContactUs> */}
-    {/* <FAQ></FAQ> */}
-    {/* <Guarantees></Guarantees> */}
-    {/* <Test></Test> */}
-    {/* </Route>
-      </Switch>
-    </Router> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/contact" element={<ContactUs></ContactUs>}></Route>
+        <Route path='/faq' element={<FAQ></FAQ>}></Route>
+        <Route path='/guarantees' element={<Guarantees></Guarantees>}></Route>
+        <Route path='/howitworks' element={<HowItWorks></HowItWorks>}></Route>
+        <Route path='/pricing' element={<Pricing></Pricing>}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
